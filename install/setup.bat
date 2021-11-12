663529ef264a0be30f92b894a222a18d5e0d405b
Aditya Mitra <adityaarghya0@gmail.com>
2021-11-12 13:20:48 +0530

    Update setup.bat

--- install/setup.bat
+++ install/setup.bat
@@ -1,4 +1,4 @@
-cd %userprofile%
+cd %userprofile% && c:
 winget install Git.Git --accept-package-agreements --accept-source-agreements
 git clone https://github.com/AdityaMitra5102/Lensify
 cd Lensify
