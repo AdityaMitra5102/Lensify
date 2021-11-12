a559b7489207abd8c09c191582795ab9c0112d98
Aditya Mitra <adityaarghya0@gmail.com>
2021-11-12 12:21:36 +0530

    Add files via upload

---
+++ install/setup.bat
@@ -0,0 +1,6 @@
+cd %userprofile%
+winget install Git.Git --accept-package-agreements --accept-source-agreements
+git clone https://github.com/AdityaMitra5102/Lensify
+cd Lensify
+cd install
+installer.bat
