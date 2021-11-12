1ba3b13b2915acd0a578e223f12b67dfb73782fc
Aditya Mitra <adityaarghya0@gmail.com>
2021-11-12 12:00:56 +0530

    Add files via upload

---
+++ install/installer.bat
@@ -0,0 +1,13 @@
+winget install python --accept-package-agreements --accept-source-agreements
+python get-pip.py
+winget install -e --id Kitware.CMake --accept-package-agreements --accept-source-agreements
+winget install 9NHL4NSC67WM --accept-package-agreements --accept-source-agreements
+pip install numpy
+pip install opencv-python
+pip install opencv-contrib-python==3.4.0.12
+pip install pillow
+pip install cmake
+pip install https://files.pythonhosted.org/packages/04/80/cad93b40262f5d09f6de82adbee452fd43cdff60830b56a74c5930f7e277/wheel-0.37.0-py2.py3-none-any.whl
+vs_community__346058749.1636694321__rid.30005.exe
+pip install dlib
+call shortcutJS.bat -linkfile "%userprofile%/Desktop/Lensify.lnk" -target "%cd%/../src/openhere.bat"
