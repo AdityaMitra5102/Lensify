dc23d99da9cf4c779fe4f2b57d9dbd04173bd72d
Aditya Mitra <adityaarghya0@gmail.com>
2021-11-13 16:46:14 +0530

    Update installer.bat

--- install/installer.bat
+++ install/installer.bat
@@ -11,5 +11,5 @@ pip install cmake
 pip install https://files.pythonhosted.org/packages/04/80/cad93b40262f5d09f6de82adbee452fd43cdff60830b56a74c5930f7e277/wheel-0.37.0-py2.py3-none-any.whl
 vs_community__346058749.1636694321__rid.30005.exe
 pip install dlib
-call shortcutJS.bat -linkfile "%userprofile%/Desktop/Lensify.lnk" -target "%cd%/../src/openhere.bat"
+call shortcutJS.bat -linkfile "%userprofile%/Desktop/Lensify.lnk" -target "%cd%/../src/openhere.bat" &
 call shortcutJS.bat -linkfile "%userprofile%/OneDrive/Desktop/Lensify.lnk" -target "%cd%/../src/openhere.bat"
